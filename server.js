dc0e1a4ec69326bfd59b05d0200ed91421def402
Katoromic <katdesigns14@gmail.com>
2024-02-29 21:31:31 -0500

    fixed CORS bug

--- server.js
+++ server.js
@@ -7,18 +7,15 @@ require('dotenv').config();
 const url = process.env.MONGODB_URI;
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
-
 client.connect(console.log("mongodb connected"));
-
-var cardList = [];
-          
+app.use(cors());
+   
 const PORT = process.env.PORT || 5000;  
-
-
 const app = express();
 app.set('port', (process.env.PORT || 5000));
-app.use(cors());
 app.use(bodyParser.json());
+
+var cardList = []; 
 
 app.post('/api/addcard', (req, res, next) => {
     // incoming: userId, color
