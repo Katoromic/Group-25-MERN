caa1180021f9c0e6584c4550c75ce196f274a151
Katoromic <katdesigns14@gmail.com>
2024-02-29 22:18:41 -0500

    CORS fix attempt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const path = require("path");
+const cors = require('cors');
 
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
