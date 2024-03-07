917b08dd52a92df52078dd2c8050dd21de1e7a9d
Paul P <pa764915@ucf.edu>
2024-03-07 16:13:49 -0500

    Removed cards endpoints

--- api.js
+++ api.js
@@ -3,36 +3,6 @@ require("mongodb");
 require("bcrypt");
 
 exports.setApp = function (app, client, bcrypt) {
-  app.post("/api/addcard", async (req, res, next) => {
-    // incoming: userId, color
-    // outgoing: error
-    const { userId, card, jwtToken } = req.body;
-    try {
-      if (token.isExpired(jwtToken)) {
-        var r = { error: "The JWT is no longer valid", jwtToken: "" };
-        res.status(200).json(r);
-        return;
-      }
-    } catch (e) {
-      console.log(e.message);
-    }
-    const newCard = { Card: card, UserId: userId };
-    var error = "";
-    try {
-      const db = client.db();
-      const result = db.collection("Cards").insertOne(newCard);
-    } catch (e) {
-      error = e.toString();
-    }
-    var refreshedToken = null;
-    try {
-      refreshedToken = token.refresh(jwtToken);
-    } catch (e) {
-      console.log(e.message);
-    }
-    var ret = { error: error, jwtToken: refreshedToken };
-    res.status(200).json(ret);
-  });
 
   // Login
   //
@@ -163,39 +133,6 @@ exports.setApp = function (app, client, bcrypt) {
     res.status(200).json(ret);
   });
 
-  app.post("/api/searchcards", async (req, res, next) => {
-    // incoming: userId, search
-    // outgoing: results[], error
-    var error = "";
-    const { userId, search, jwtToken } = req.body;
-    try {
-      if (token.isExpired(jwtToken)) {
-        var r = { error: "The JWT is no longer valid", jwtToken: "" };
-        res.status(200).json(r);
-        return;
-      }
-    } catch (e) {
-      console.log(e.message);
-    }
-    var _search = search.trim();
-    const db = client.db();
-    const results = await db
-      .collection("Cards")
-      .find({ Card: { $regex: _search + ".*", $options: "i" } })
-      .toArray();
-    var _ret = [];
-    for (var i = 0; i < results.length; i++) {
-      _ret.push(results[i].Card);
-    }
-    var refreshedToken = null;
-    try {
-      refreshedToken = token.refresh(jwtToken);
-    } catch (e) {
-      console.log(e.message);
-    }
-    var ret = { results: _ret, error: error, jwtToken: refreshedToken };
-    res.status(200).json(ret);
-  });
 
   // Process email verification link
   //
