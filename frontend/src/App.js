19483c218736418e016e0ab36d67e1676fece40d
giullyasantos <123971723+giullyasantos@users.noreply.github.com>
2024-03-08 00:05:42 -0500

    Added Login Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import LoginForm from './pages/LoginForm';
 
 function App() {
   return (
