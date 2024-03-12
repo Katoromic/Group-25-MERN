5d0ede3632da0ff9a58170f9039f32e1057dd05e
Williams <wlms6646@DESKTOP-DU65CNO>
2024-03-12 16:26:07 -0400

    Resolve conflict

--- authenticationHandler.js
+++ authenticationHandler.js
@@ -1,6 +1,7 @@
 // Vefify
 
 require('dotenv').config();
+module.exports = sendVerificationEmail;
 
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
@@ -12,25 +13,45 @@ const emailSender = nodemailer.createTransport({
     }
 });
 
-
-
 const token = jwt.sign({
     data: ''
 }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' }
 );
+function createMailOptions(toEmail) {
+    return {
+        from: "senseijake24@gmail.com",
+        to: toEmail,
+        subject: 'Verify Your Email Address',
+        html: `Hello! You recently signed up for an account with our website. Please follow the link below to verify your email
+    <a href="http://localhost:5005/verify/${token}">http://localhost:5005/verify/${token}</a>`
+    }
+}
 
+function sendVerificationEmail(user) {
+    const toEmail = user.Email;
+    emailSender.sendMail(createMailOptions(toEmail), function (error, info) {
+        if (error) throw Error(error);
+        console.log('Email Sent');
+        console.log(info);
+    });
+}
+
+/*
+Testing Code
 const mailOptions = {
     from: "senseijake24@gmail.com",
     to: "senseijake24@gmail.com",
+    //to: toEmail,
     subject: 'Verify Your Email Address',
     html: `Hello! You recently signed up for an account with our website. Please follow the link below to verify your email
     <a href="http://localhost:5005/verify/${token}">http://localhost:5005/verify/${token}</a>`
 };
 
+user = {
+    Email: "senseijake24@gmail.com"
+}
+sendVerificationEmail(user);
 
 
-emailSender.sendMail(mailOptions, function (error, info) {
-    if (error) throw Error(error);
-    console.log('Email Sent');
-    console.log(info);
-});
+
+*/
