b01a93e4dfcffa3c56455f72ee83f701b6664942
DennisKlingener <149022940+DennisKlingener@users.noreply.github.com>
2024-04-05 20:44:05 -0400

    added conditional messages

--- frontend/src/components/ForgotPasswordForm.js
+++ frontend/src/components/ForgotPasswordForm.js
@@ -9,6 +9,10 @@ const ForgotPasswordForm = () => {
     // Stores the email and username?
     const [RecoveryData, SetRecoveryData] = useState ({username: '', email: ''});
 
+    // Conditional display
+    const [SuccessMessage, SetSuccessMessage] = useState(false);
+    const [FailMessage, SetFailMesage] = useState(false);
+
     // Path builder
     var bp = require("./Path.js");
 
@@ -45,6 +49,9 @@ const ForgotPasswordForm = () => {
                     <div id='FormParentElement' >
                         
                         <h1 className='mb-4'>Password Recovery</h1>
+
+                        {SuccessMessage && <h2 id='SuccessMessage' className='m-3'>Recovery successful! Please check your email.</h2>}
+                        {FailMessage && <h2 id='FailMessage' className='m-3'>Uh oh something went wrong...</h2>}
 
                         <input type='text' name='username' value={RecoveryData.username} onChange={HandleInputChange} placeholder='Username' className='Input-Box mb-4'/>
 
