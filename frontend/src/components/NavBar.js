559d5df880c3384a9cac886d073c9cc0818ab753
giullyasantos <123971723+giullyasantos@users.noreply.github.com>
2024-04-09 08:45:13 -0400

    making navbar responsive and connecting login icon and shuriken to correct pages

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,18 +11,26 @@ function NavBar() {
         <div id='NavBarBackGround'>
             <nav className='navbar navbar-default bg-customNavBar'>
                 <div className='container-fluid'>
-                    <div className='d-flex align-items-center justify-content-end w-100'>
-                        <img className='shuriken' src={ShurikenX}/>
-                        <div class= 'home-underline-animation'>
-                             <HomeButton />
+                    <div className='d-flex align-items-center justify-content-between w-100'> {/* justify-content-between to distribute items along the main axis */}
+                        <div>
+                            <a href="/">
+                                <img className='shuriken' src={ShurikenX}/>
+                            </a>
                         </div>
-                        <div class= 'github-underline-animation'>
-                            <GitHubButton />
+                        <div className='d-flex flex-row align-items-end'> {/* Align items at the end in a column */}
+                            <div className='home-underline-animation'>
+                                <HomeButton />
+                            </div>
+                            <div className='github-underline-animation'>
+                                <GitHubButton />
+                            </div>
+                            <div className='aboutus-underline-animation'>
+                                <AboutUsButton />
+                            </div>
+                            <a href="/login">
+                                <FaUserAlt className='profileIcon' />
+                            </a>
                         </div>
-                        <div class= 'aboutus-underline-animation'>
-                            <AboutUsButton />
-                        </div>
-                        <FaUserAlt className='profileIcon' />
                     </div>
                 </div>
             </nav>
