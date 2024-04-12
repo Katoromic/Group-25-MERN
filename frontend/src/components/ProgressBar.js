710e6588b0d4915f62aa9f4e46af965b1f7266f5
DennisKlingener <149022940+DennisKlingener@users.noreply.github.com>
2024-04-12 17:18:45 -0400

    changing background color

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -9,7 +9,7 @@ const ProgressBar = ({Progress}) => {
             <div className='row'>
                 <div className='col'>
                     <div id='ProgressContainer' className='progress mt-3 mb-3' style={{height: '25px'}}>
-                        <div id='ProgressBar' className='progress-bar bg-danger progress-bar-striped progress-bar-animated overflow-visible text-dark' style={{width: `${Progress}%`}} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                        <div id='ProgressBar' className='progress-bar overflow-visible text-dark' style={{width: `${Progress}%`}} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
                             <span>{Progress}%</span>
                         </div>
                     </div>
