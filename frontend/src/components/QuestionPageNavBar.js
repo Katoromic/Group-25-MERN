a25f1423b50e24c891acf1fa864e9046b52c9238
giullyasantos <123971723+giullyasantos@users.noreply.github.com>
2024-04-15 15:18:48 -0400

    Update QuestionPageNavBar.js

--- frontend/src/components/QuestionPageNavBar.js
+++ frontend/src/components/QuestionPageNavBar.js
@@ -14,7 +14,7 @@ const QuestionPageNavBar = ({Progress, QuestionsCorrect, QuestionsIncorrect, Use
         <div id='QuestionPageNavElement'>
 
             <div className='container-fluid'>
-        
+                <DashBoardButton QuestionsCorrect={QuestionsCorrect} LanguageName={LanguageName} CurrentQuestion={CurrentQuestion} UserTokenRaw={UserTokenRaw}/>
                 <div className='row'>
 
                     <div id='UsersName' className='col d-flex align-items-center justify-content-center'>
@@ -35,13 +35,9 @@ const QuestionPageNavBar = ({Progress, QuestionsCorrect, QuestionsIncorrect, Use
                 </div>
                 
                 <div className='d-grid gap-2 mx-auto'>
-                    <HintButton />
-                    <DashBoardButton QuestionsCorrect={QuestionsCorrect} LanguageName={LanguageName} CurrentQuestion={CurrentQuestion} UserTokenRaw={UserTokenRaw}/>
                     <SaveLogoutButton QuestionsCorrect={QuestionsCorrect} LanguageName={LanguageName} CurrentQuestion={CurrentQuestion} UserTokenRaw={UserTokenRaw}/>
                 </div>
-
             </div>
-
         </div>
     );
 };
